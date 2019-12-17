1211b61ce6f7b5e726d896a18455678d7a1de3b9
lazerblimp <57571370+lazerblimp@users.noreply.github.com>
2019-12-17 12:12:31 -0800

    Lots of stuff

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    public Vector3 _cameraOffset;
+
+    public float cameraSpeed = 10.0f;
+
+    public float smoothFactor = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+   
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+   
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float speed = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject instBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidBody.AddForce(transform.forward * speed);
+        }
+	}
+}
